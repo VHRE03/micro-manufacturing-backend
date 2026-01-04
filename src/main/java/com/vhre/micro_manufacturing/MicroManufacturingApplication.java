0183e7475c3f4b48c73196bffed04f0be9c024f7
VHRE03 <vhre03@gmail.com>
2026-01-04 15:51:58 -0600

    first commit

---
+++ src/main/java/com/vhre/micro_manufacturing/MicroManufacturingApplication.java
@@ -0,0 +1,13 @@
+package com.vhre.micro_manufacturing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroManufacturingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroManufacturingApplication.class, args);
+	}
+
+}
